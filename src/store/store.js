3a2581599eb5495d7d3961a7966dcb840451aba1
April Day <april.day@gmail.com>
2021-06-22 19:56:47 +0100

    seventh commit

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
-import Vue from 'vue',
-import Vuex from 'vuex',
+import Vue from 'vue'
+import Vuex from 'vuex'
 import * as fb from '../firebase'
 import router from '../router/index'
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,4 +1,31 @@
 <template>
+    <div>          
+        <form>
+            
+                <h1>get started</h1>
+            <div>  
+                <label for="name">Name</label> 
+                    <input v-model.trim="signupForm.name" type="text" placeholder="your name here" id="name"> 
+            </div>
+            <div>
+                <label for="title">title</label>
+                <input v-model.trim="signupForm.title" type="text" placeholder="Company" id="title" />
+            </div>
+                <label for="email2">email</label>
+                <input v-model.trim="signupForm.email" type="text" placholder="you@email.com" id="email2" />
+            <div>
+                <label for="password2">password</label>
+                <input v-model.trim="signupForm.password" type="password" placeholder="your password (min 6 letters)" id="password2" />
+            </div>
+            <button @click="signup()" class="button">sign up</button>
+            <div class="extras">
+                <a>Back to Log in </a>                
+            </div>
+            <div>
+            </div>
+        </form>
+    </div>
+
     <div id="login">
     <section>
     <div class="col1">
@@ -6,15 +33,15 @@
         <p>Welcome to the <a href="https://savvyapps.com" target="_blank">Savvy Apps</a> sample social web app </p>
     </div>
     <div class="col12">
-        <form>
+        <form @submit.prevent>
             <h1>welcome back</h1>
             <div>
                 <label for="email1">email</label>
-                <input type="text" placeholder="your@email.com" id="email1" />
+                <input v-model.trim= "loginForm.email" type="text" placeholder="your@email.com" id="email1" />
             </div><!-- end email label -->
             <div>
                 <label for="password1">password</label> 
-                <input type="password" placeholder="*****" id="password1"/>
+                <input v-model.trim="loginForm.password" type="password" placeholder="*****" id="password1"/>
             </div>
             <button @click="login()" class="button">log in</button>
             <div class="extras">
@@ -35,11 +62,25 @@
                     email: this.loginForm.email,
                     password: this.loginForm.password
                 })
+            },
+            signup(){
+                this.$store.dispatch('signup', {
+                    email: this.signupForm.email,
+                    password: this.signupForm.password,
+                    name: this.signupForm.name,
+                    title: this.signupForm.title
+                })
             }
         },
         data() {
             return {
                 loginForm: {
+                    email: '',
+                    password: ''
+                },
+                signupForm: {
+                    name: '',
+                    title: '',
                     email: '',
                     password: ''
                 }
