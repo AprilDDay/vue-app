03e581a7c854acd1ac54e23b0951374a2483edf2
April Day <april.day@gmail.com>
2021-06-24 09:02:49 +0100

    ninth commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,25 @@
 import { createStore } from 'vuex'
 
+
 //need to put action handlers
 export default createStore({
   state: {
+  },
+  method: {
+    async signup({dispatch}, form) {
+      //sign user up
+      const { const } = await fb.auth.createWithEmailAndPassword(form.email, form.password)
+    
+      //create user profile in userCollections
+      await fb.usersCollection.doc(user.uid).set({
+        name: form.name,
+        title: form.title
+      })
+    
+      //fetch user profile and set in state
+      dispatch('fetchUserProfile', user)
+    }
+    
   },
   mutations: {
   },
