6b9c971af32f599a5f6f22510275384bbebc7eeb
April Day <april.day@gmail.com>
2021-06-29 10:41:26 +0100

    sixteenth commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
 <template>
   <div id="app">
-    <h1>Love Me Tender, Love Me True</h1>
+    <SiteNav v-if="showNav"></SiteNav>
+<!--    <h1>Love Me Tender, Love Me True</h1> -->
     <router-view />
   </div>
 </template>
+
+<script>
+
+import { mapState } from 'vuex'
+import SiteNav from '@/components/SiteNav'
+
+export default {
+  components: {
+    SiteNav
+  },
+  computed: {
+    ..mapState(['userProfile']),
+    showNav() {
+      return Object.keys(this.userProfile).length > 1
+    }
+  }
+}
+
+//following was auto put 
+/*import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+*/
+
+</script>
